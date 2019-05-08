8e8b50d67fa613cfa313fe955c756d9a778d103c
Julien Roncaglia <julien@roncaglia.fr>
2019-05-08 14:06:54 +0200

    Upgrade to Fable 2 in package managers

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
 var path = require("path");
 var webpack = require("webpack");
-var fableUtils = require("fable-utils");
-var nodeExternals = require('webpack-node-externals');
 
 function resolve(filePath) {
   return path.join(__dirname, filePath)
 }
 
-
-var babelOptions = fableUtils.resolveBabelOptions({
+var babelOptions = {
   presets: [["es2015", { "modules": false }]],
   plugins: ["transform-runtime"]
-});
+};
 
 var isProduction = process.argv.indexOf("-p") >= 0;
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
@@ -34,13 +31,8 @@ module.exports = function(env) {
   output: {
     filename: 'fsharp.js',
     path: resolve('./' + outputPath),
-    //library: 'IONIDEFSHARP',
     libraryTarget: 'commonjs'
   },
-  resolve: {
-    modules: [resolve("./node_modules/")]
-  },
-  //externals: [nodeExternals()],
   externals: {
     // Who came first the host or the plugin ?
     "vscode": "commonjs vscode",
